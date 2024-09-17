eee1a074879fb5e76c8abd1dd6a4c0d3f29a673b
paradoxe-tech <89200698+paradoxe-tech@users.noreply.github.com>
2024-09-17 11:13:50 +0200

    load inputs in model from files

--- model/Model.py
+++ model/Model.py
@@ -1,11 +1,17 @@
 import esm
 import torch
+from torch.utils.data import DataLoader
+
 from pocketgen.models.PD import Pocket_Design_new
 from pocketgen.utils.misc import seed_all, load_config
 from pocketgen.utils.transforms import FeaturizeProteinAtom, FeaturizeLigandAtom
+from pocketgen.utils.data import collate_mols_block
+from functools import partial
+
+from .sampler import interaction
 
 class Model:
-  def __init__(self, checkpoint_path:str, verbose:int=1) -> "Model":
+  def __init__(self, checkpoint_path:str, verbose:int=1, device="cuda:0") -> "Model":
     """
     The mutant generation model constructor. This method does the setup of 
     torch and CUDA environment, loads the checkpoint and then returns a PocketGen 
@@ -20,6 +26,8 @@ class Model:
     self.pwd = "./"
     self.output_path = "./results"
     self.mutants = []
+    self.config = load_config('./pocketgen/configs/train_model.yml')
+    self.device = device
 
     if self.verbose > 0:
       print('__PJNAME__ setup started, please wait.')
@@ -29,17 +37,18 @@ class Model:
     # clean cache and setting the libs seeds
     torch.cuda.empty_cache()
     seed_all(2089)
-    self.device = torch.device('cpu') # for GPU : "cuda:0"
 
     if self.verbose == 2:
-      print('\ttorch and CUDA initialized correctly.\nNow retrieving alphabet from fair-ESM :')
+      print('\tpytorch and CUDA initialized correctly.')
+      print('Now retrieving alphabet from fair-ESM :')
 
     # set ESM2 alphabet as the usual alphabet
-    _, self.alphabet = esm.pretrained.load_model_and_alphabet_hub('esm2_t33_650M_UR50D')
-    del _ # ESM2 pretrained_model that we don't need here is deleted from memory
+    pretrained_model, self.alphabet = esm.pretrained.load_model_and_alphabet_hub('esm2_t33_650M_UR50D')
+    del pretrained_model # ESM2 pretrained_model that we don't need here is deleted from memory
 
     if self.verbose == 2:
-      print('\tESM alphabet successfully loaded.\nNow building PocketGen model :')
+      print('\tESM alphabet successfully loaded.')
+      print('Now building PocketGen model :')
 
     # set the model and load the checkpoint from .pt file
     self.checkpoint = torch.load(checkpoint_path, map_location=self.device)
@@ -48,14 +57,14 @@ class Model:
       print('\tcheckpoint successfully created.')
 
     self.model = Pocket_Design_new(
-      load_config('./pocketgen/configs/train_model.yml').model,
+      self.config.model,
       protein_atom_feature_dim=FeaturizeProteinAtom().feature_dim,
       ligand_atom_feature_dim=FeaturizeLigandAtom().feature_dim,
       device=self.device
     )
 
     if self.verbose == 2:
-        print("\tPocketGen model well instanciated.")
+      print("\tPocketGen model well instanciated.")
 
     self.model = self.model.to(self.device)
 
@@ -65,18 +74,49 @@ class Model:
     self.model.load_state_dict(self.checkpoint['model'])
 
     if self.verbose == 2:
-      print('\tcheckpoint loaded into PocketGen.\nEnd of setup, model can now be used.\n\n\n')
+      print('\tcheckpoint loaded into PocketGen.\n')
+      print('End of setup, model can now be used.\n\n\n')
 
     return self
   
 
-  def input(self, receptor_path, ligand_path):
-    pass
+  def input(self, receptor_path:str, ligand_path:str) -> "Model":
+    """
+    Loads a protein receptor and a ligand from files and store it in 
+    a data-loader, useable by the model when generating mutants.
+    @param ligand_path (str): path to the ligand SDF file.
+    @param receptor_path (str): path to the receptor PDB file.
+    @return (Model): the instance of Model, for chainability purposes.
+    """
 
+    if self.verbose == 2:
+      print('Now parsing data from receptor and ligand :')
+    
+    # get dense features from receptor-ligand interaction
+    features = interaction(receptor_path, ligand_path)
 
+    if self.verbose == 2:
+      print('\tsuccessfully parsed interaction features.\n')
+      print('Now building the pytorch dataloader :')
+
+    self.loader = DataLoader(
+      [features for _ in range(8)], # 8 * features for batching reasons
+      batch_size=4, 
+      shuffle=False,
+      num_workers=self.config.train.num_workers,
+      collate_fn=partial(
+        collate_mols_block, 
+        batch_converter=self.alphabet.get_batch_converter()
+      )
+    )
+
+    if self.verbose == 2:
+      print('\tpytorch dataloader built correctly.')
+
+  
   def generate(self):
     pass
-
+    
   
   def results(self):
     pass
