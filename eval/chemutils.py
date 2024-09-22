5af57aea39b74ec8a8a58f0336bb72ce6279a8d8
paradoxe-tech <kitsu.tnh.contact@gmail.com>
2024-09-22 16:14:15 +0200

    converted kcal/mol to J/mol

--- eval/chemutils.py
+++ eval/chemutils.py
@@ -1,11 +1,21 @@
 import math
 
-def kd(delta_G:float, temperature:float=298) -> float:
+def kcalmol_to_jmol(x:float):
+  """
+  converts an energy from kcal/mol to J/mol
+  @x (float): energy value to convert (kcal/mol)
+  @return (float): converted energy value (J/mol)
+  """
+
+  return x * 4184.0
+
+
+def kd(delta_G:float, temperature:float=298.0) -> float:
   """
   calculates the affinity constant depending on the binding free energy
-  @param delta_G: the value of the binding free energy
-  @param temperature: temperature, using Kelvin degrees
-  @return: affinity constant (alias Kd)
+  @param delta_G (float): the value of the binding free energy (kcal/mol)
+  @param temperature (float): temperature (kelvin)
+  @return (float): affinity constant
   """
   
-  return math.exp(delta_G / (8.314 * temperature))
+  return math.exp((-1 * kcalmol_to_jmol(delta_G)) / (8.314 * temperature))
